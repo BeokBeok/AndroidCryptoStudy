659097f90b180f7014a03d60f95c4058eb7b4a78
kekemusa <kekemusa37@gmail.com>
2019-06-18 10:39:58 +0900

    [Crypto] 패키지명 변경

---
+++ moaAndroidCryptoCoreAPI/src/main/java/org/moa/android/crypto/coreapi/MoaBase58.java
@@ -0,0 +1,89 @@
+package org.moa.android.crypto.coreapi;
+
+import android.util.Log;
+
+import java.math.BigInteger;
+
+public class MoaBase58 {
+    private static final String ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    private static final BigInteger BASE = BigInteger.valueOf(58);
+
+    private MoaBase58() {
+    }
+
+    public static MoaBase58 getInstance() {
+        return Singleton.instance;
+    }
+
+    /**
+     * 바이트 배열을 입력 받아 Base58로 엔코딩하여 문자열로 반환한다.
+     *
+     * @param input MoaBase58 문자열로 변환할 바이트 배열 입력
+     * @return String MoaBase58 변환 문자열 반환
+     */
+    public String encode(byte[] input) {
+        // This could be a lot more efficient.
+        BigInteger bigInteger = new BigInteger(1, input);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (bigInteger.compareTo(BASE) >= 0) {
+            BigInteger mod = bigInteger.mod(BASE);
+            stringBuilder.insert(0, ALPHABET.charAt(mod.intValue()));
+            bigInteger = bigInteger.subtract(mod).divide(BASE);
+        }
+
+        stringBuilder.insert(0, ALPHABET.charAt(bigInteger.intValue()));
+
+        // Convert leading zeros too.
+        for (byte anInput : input) {
+            if (anInput == 0)
+                stringBuilder.insert(0, ALPHABET.charAt(0));
+            else
+                break;
+        }
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Base58로 엔코딩된 문자열을 받아 MoaBase58 디코딩한 바이트 배열을 반환한다.
+     *
+     * @param input Base58로 엔코딩된 문자열 입력
+     * @return byte[] Base58로 디코딩된 바이트 배열 반환
+     */
+    public byte[] decode(String input) {
+        byte[] bytes = decodeToBigInteger(input).toByteArray();
+
+        boolean stripSignByte = bytes.length > 1 && bytes[0] == 0 && bytes[1] < 0;
+
+        // Count the leading zeros, if any.
+        int leadingZeros = 0;
+        for (int i = 0; input.charAt(i) == ALPHABET.charAt(0); i++)
+            leadingZeros++;
+
+        // Now cut/pad correctly. Java 6 has a convenience for this, but Android can't use it.
+        byte[] tmp = new byte[bytes.length - (stripSignByte ? 1 : 0) + leadingZeros];
+        System.arraycopy(bytes, stripSignByte ? 1 : 0, tmp, leadingZeros, tmp.length - leadingZeros);
+
+        return tmp;
+    }
+
+    private BigInteger decodeToBigInteger(String input) {
+        BigInteger bigInteger = BigInteger.valueOf(0);
+
+        // Work backwards through the string.
+        for (int i = input.length() - 1; i >= 0; i--) {
+            int alphaIndex = ALPHABET.indexOf(input.charAt(i));
+            if (alphaIndex == -1) {
+                Log.d("MoaLib", "[MoaBase58][decodeToBigInteger] alpha index is -1");
+                return BigInteger.ZERO;
+            }
+            bigInteger = bigInteger.add(BigInteger.valueOf(alphaIndex).multiply(BASE.pow(input.length() - 1 - i)));
+        }
+        return bigInteger;
+    }
+
+    private static class Singleton {
+        private static MoaBase58 instance = new MoaBase58();
+    }
+}
---
+++ moaAndroidCryptoCoreAPI/src/main/java/org/moa/android/crypto/coreapi/Symmetric.java
@@ -0,0 +1,62 @@
+package org.moa.android.crypto.coreapi;
+
+import android.util.Log;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.StringTokenizer;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Symmetric {
+    private Cipher cipher;
+    private IvParameterSpec ivSpec;
+    private SecretKeySpec keySpec;
+    private String modeType;
+
+    public Symmetric(String cryptoNameModePadType, byte[] ivBytes, byte[] keyBytes) {
+        try {
+            StringTokenizer stringTokenizer = new StringTokenizer(cryptoNameModePadType, "/");
+            String cryptoAlgName = stringTokenizer.nextToken();
+            modeType = stringTokenizer.nextToken();
+            cipher = Cipher.getInstance(cryptoNameModePadType);
+            int blockSize = cipher.getBlockSize();
+            int keySize = keyBytes.length;
+
+            if (blockSize != keySize && blockSize + 8 != keySize && blockSize + 16 != keySize) {
+                Log.d("MoaLib", "[Symmetric]" + "Invalid key size error -> using 128/192/256bit");
+                return;
+            }
+
+            keySpec = new SecretKeySpec(keyBytes, cryptoAlgName);
+            if (!modeType.equals("ECB"))
+                ivSpec = new IvParameterSpec(ivBytes);
+
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
+            Log.d("MoaLib", "[Symmetric]" + e.getMessage());
+        }
+    }
+
+    public byte[] getSymmetricData(int encOrDecMode, byte[] data) {
+        byte[] result = {0, };
+        if (data.length == 0)
+            return result;
+        try {
+            if (modeType.equals("ECB"))
+                cipher.init(encOrDecMode, keySpec);
+
+            cipher.init(encOrDecMode, keySpec, ivSpec);
+            result = cipher.doFinal(data);
+            return result;
+        } catch (InvalidKeyException | InvalidAlgorithmParameterException | BadPaddingException | IllegalBlockSizeException e) {
+            Log.d("MoaLib", "[Symmetric][getSymmetricData]" + e.getMessage());
+            return new byte[0];
+        }
+    }
+}
